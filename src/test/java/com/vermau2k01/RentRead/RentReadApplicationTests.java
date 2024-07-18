8cdd7428b6615d91bb1e68701926e7620af729b5
Utkarsh <vermau2k01@gmail.com>
2024-07-18 21:08:17 +0530

    Initial commit

---
+++ src/test/java/com/vermau2k01/RentRead/RentReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vermau2k01.RentRead;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentReadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
